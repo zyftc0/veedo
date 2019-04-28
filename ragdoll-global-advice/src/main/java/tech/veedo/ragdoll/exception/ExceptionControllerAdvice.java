71b36db8092b12da7ec4a33ae471328cb82305d5
张 一夫 <qwer1234=>
2019-04-28 11:39:11 +0800

    更新文件

--- ragdoll-global-advice/src/main/java/tech/veedo/ragdoll/exception/ExceptionControllerAdvice.java
+++ ragdoll-global-advice/src/main/java/tech/veedo/ragdoll/exception/ExceptionControllerAdvice.java
@@ -1,5 +1,6 @@
 package tech.veedo.ragdoll.exception;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -16,6 +17,213 @@ public class ExceptionControllerAdvice extends ResponseEntityExceptionHandler {
     public ExceptionAdviceEntity globalExceptionHanlder(GlobalException ex) {
         ex.printStackTrace();
         return generateResponseTMP(ex.getErrorCode(), ex.getDetailMessage());
+    }
+
+    @ExceptionHandler(ArithmeticException.class)
+    @ResponseBody
+    public ExceptionAdviceEntity arithmeticExceptionHanlder(ArithmeticException ex) {
+        ex.printStackTrace();
+        return generateResponseTMP(GlobalExceptionCode.ARITHMETIC.getErrorCode(),
+                StringUtils.isBlank(ex.getMessage())?GlobalExceptionCode.ARITHMETIC.getDetailMessage():ex.getMessage());
+    }
+
+    @ExceptionHandler(ArrayIndexOutOfBoundsException.class)
+    @ResponseBody
+    public ExceptionAdviceEntity arrayIndexOutOfBoundsExceptionHanlder(ArrayIndexOutOfBoundsException ex) {
+        ex.printStackTrace();
+        return generateResponseTMP(GlobalExceptionCode.ARRAY_INDEX_OUT_OF_BOUNDS.getErrorCode(),
+                StringUtils.isBlank(ex.getMessage())?GlobalExceptionCode.ARRAY_INDEX_OUT_OF_BOUNDS.getDetailMessage():ex.getMessage());
+    }
+
+    @ExceptionHandler(ArrayStoreException.class)
+    @ResponseBody
+    public ExceptionAdviceEntity arrayStoreExceptionHanlder(ArrayStoreException ex) {
+        ex.printStackTrace();
+        return generateResponseTMP(GlobalExceptionCode.ARRAY_STORE.getErrorCode(),
+                StringUtils.isBlank(ex.getMessage())?GlobalExceptionCode.ARRAY_STORE.getDetailMessage():ex.getMessage());
+    }
+
+    @ExceptionHandler(ClassCastException.class)
+    @ResponseBody
+    public ExceptionAdviceEntity classCastExceptionHanlder(ClassCastException ex) {
+        ex.printStackTrace();
+        return generateResponseTMP(GlobalExceptionCode.CLASS_CAST.getErrorCode(),
+                StringUtils.isBlank(ex.getMessage())?GlobalExceptionCode.CLASS_CAST.getDetailMessage():ex.getMessage());
+    }
+
+    @ExceptionHandler(ClassNotFoundException.class)
+    @ResponseBody
+    public ExceptionAdviceEntity classNotFoundExceptionHanlder(ClassNotFoundException ex) {
+        ex.printStackTrace();
+        return generateResponseTMP(GlobalExceptionCode.CLASS_NOT_FOUND.getErrorCode(),
+                StringUtils.isBlank(ex.getMessage())?GlobalExceptionCode.CLASS_NOT_FOUND.getDetailMessage():ex.getMessage());
+    }
+
+    @ExceptionHandler(CloneNotSupportedException.class)
+    @ResponseBody
+    public ExceptionAdviceEntity cloneNotSupportedExceptionHanlder(CloneNotSupportedException ex) {
+        ex.printStackTrace();
+        return generateResponseTMP(GlobalExceptionCode.CLONE_NOT_SUPPORTED.getErrorCode(),
+                StringUtils.isBlank(ex.getMessage())?GlobalExceptionCode.CLONE_NOT_SUPPORTED.getDetailMessage():ex.getMessage());
+    }
+
+    @ExceptionHandler(EnumConstantNotPresentException.class)
+    @ResponseBody
+    public ExceptionAdviceEntity enumConstantNotPresentExceptionHanlder(EnumConstantNotPresentException ex) {
+        ex.printStackTrace();
+        return generateResponseTMP(GlobalExceptionCode.ENUM_CONSTANT_NOT_PRESENT.getErrorCode(),
+                StringUtils.isBlank(ex.getMessage())?GlobalExceptionCode.ENUM_CONSTANT_NOT_PRESENT.getDetailMessage():ex.getMessage());
+    }
+
+    @ExceptionHandler(IllegalAccessException.class)
+    @ResponseBody
+    public ExceptionAdviceEntity illegalAccessExceptionHanlder(IllegalAccessException ex) {
+        ex.printStackTrace();
+        return generateResponseTMP(GlobalExceptionCode.ILLEGAL_ACCESS.getErrorCode(),
+                StringUtils.isBlank(ex.getMessage())?GlobalExceptionCode.ILLEGAL_ACCESS.getDetailMessage():ex.getMessage());
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    @ResponseBody
+    public ExceptionAdviceEntity illegalArgumentExceptionHanlder(IllegalArgumentException ex) {
+        ex.printStackTrace();
+        return generateResponseTMP(GlobalExceptionCode.ILLEGAL_ARGUMENT.getErrorCode(),
+                StringUtils.isBlank(ex.getMessage())?GlobalExceptionCode.ILLEGAL_ARGUMENT.getDetailMessage():ex.getMessage());
+    }
+
+    @ExceptionHandler(IllegalMonitorStateException.class)
+    @ResponseBody
+    public ExceptionAdviceEntity illegalMonitorStateExceptionHanlder(IllegalMonitorStateException ex) {
+        ex.printStackTrace();
+        return generateResponseTMP(GlobalExceptionCode.ILLEGAL_MONITOR.getErrorCode(),
+                StringUtils.isBlank(ex.getMessage())?GlobalExceptionCode.ILLEGAL_MONITOR.getDetailMessage():ex.getMessage());
+    }
+
+    @ExceptionHandler(IllegalStateException.class)
+    @ResponseBody
+    public ExceptionAdviceEntity illegalStateExceptionHanlder(IllegalStateException ex) {
+        ex.printStackTrace();
+        return generateResponseTMP(GlobalExceptionCode.ILLEGAL_STATE.getErrorCode(),
+                StringUtils.isBlank(ex.getMessage())?GlobalExceptionCode.ILLEGAL_STATE.getDetailMessage():ex.getMessage());
+    }
+
+    @ExceptionHandler(IllegalThreadStateException.class)
+    @ResponseBody
+    public ExceptionAdviceEntity illegalThreadStateExceptionHanlder(IllegalThreadStateException ex) {
+        ex.printStackTrace();
+        return generateResponseTMP(GlobalExceptionCode.ILLEGAL_THREAD_STATE.getErrorCode(),
+                StringUtils.isBlank(ex.getMessage())?GlobalExceptionCode.ILLEGAL_THREAD_STATE.getDetailMessage():ex.getMessage());
+    }
+
+    @ExceptionHandler(IndexOutOfBoundsException.class)
+    @ResponseBody
+    public ExceptionAdviceEntity indexOutOfBoundsExceptionHanlder(IndexOutOfBoundsException ex) {
+        ex.printStackTrace();
+        return generateResponseTMP(GlobalExceptionCode.INDEX_OUT_OF_BOUNDS.getErrorCode(),
+                StringUtils.isBlank(ex.getMessage())?GlobalExceptionCode.INDEX_OUT_OF_BOUNDS.getDetailMessage():ex.getMessage());
+    }
+
+    @ExceptionHandler(InstantiationException.class)
+    @ResponseBody
+    public ExceptionAdviceEntity instantiationExceptionHanlder(InstantiationException ex) {
+        ex.printStackTrace();
+        return generateResponseTMP(GlobalExceptionCode.INSTANTIATION.getErrorCode(),
+                StringUtils.isBlank(ex.getMessage())?GlobalExceptionCode.INSTANTIATION.getDetailMessage():ex.getMessage());
+    }
+
+    @ExceptionHandler(InterruptedException.class)
+    @ResponseBody
+    public ExceptionAdviceEntity interruptedExceptionHanlder(InterruptedException ex) {
+        ex.printStackTrace();
+        return generateResponseTMP(GlobalExceptionCode.INTERRUPTED.getErrorCode(),
+                StringUtils.isBlank(ex.getMessage())?GlobalExceptionCode.INTERRUPTED.getDetailMessage():ex.getMessage());
+    }
+
+    @ExceptionHandler(NegativeArraySizeException.class)
+    @ResponseBody
+    public ExceptionAdviceEntity negativeArraySizeExceptionHanlder(NegativeArraySizeException ex) {
+        ex.printStackTrace();
+        return generateResponseTMP(GlobalExceptionCode.NEGATIVE_ARRAY_SIZE.getErrorCode(),
+                StringUtils.isBlank(ex.getMessage())?GlobalExceptionCode.NEGATIVE_ARRAY_SIZE.getDetailMessage():ex.getMessage());
+    }
+
+    @ExceptionHandler(NoSuchFieldException.class)
+    @ResponseBody
+    public ExceptionAdviceEntity noSuchFieldExceptionnHanlder(NoSuchFieldException ex) {
+        ex.printStackTrace();
+        return generateResponseTMP(GlobalExceptionCode.NO_SUCH_FIELD.getErrorCode(),
+                StringUtils.isBlank(ex.getMessage())?GlobalExceptionCode.NO_SUCH_FIELD.getDetailMessage():ex.getMessage());
+    }
+
+    @ExceptionHandler(NoSuchMethodException.class)
+    @ResponseBody
+    public ExceptionAdviceEntity noSuchMethodExceptionHanlder(NoSuchMethodException ex) {
+        ex.printStackTrace();
+        return generateResponseTMP(GlobalExceptionCode.NO_SUCH_METHOD.getErrorCode(),
+                StringUtils.isBlank(ex.getMessage())?GlobalExceptionCode.NO_SUCH_METHOD.getDetailMessage():ex.getMessage());
+    }
+
+    @ExceptionHandler(NullPointerException.class)
+    @ResponseBody
+    public ExceptionAdviceEntity nullPointerExceptionHanlder(HttpServletRequest request, NullPointerException ex) {
+        ex.printStackTrace();
+        return generateResponseTMP(GlobalExceptionCode.NULL_POINTER.getErrorCode(), ex.getMessage());
+    }
+
+    @ExceptionHandler(NumberFormatException.class)
+    @ResponseBody
+    public ExceptionAdviceEntity numberFormatExceptionHanlder(NumberFormatException ex) {
+        ex.printStackTrace();
+        return generateResponseTMP(GlobalExceptionCode.NUMBER_FORMAT.getErrorCode(),
+                StringUtils.isBlank(ex.getMessage())?GlobalExceptionCode.NUMBER_FORMAT.getDetailMessage():ex.getMessage());
+    }
+
+    @ExceptionHandler(ReflectiveOperationException.class)
+    @ResponseBody
+    public ExceptionAdviceEntity reflectiveOperationExceptionHanlder(ReflectiveOperationException ex) {
+        ex.printStackTrace();
+        return generateResponseTMP(GlobalExceptionCode.REFLECTIVE_OPERATION.getErrorCode(),
+                StringUtils.isBlank(ex.getMessage())?GlobalExceptionCode.REFLECTIVE_OPERATION.getDetailMessage():ex.getMessage());
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    @ResponseBody
+    public ExceptionAdviceEntity runtimeExceptionHanlder(RuntimeException ex) {
+        ex.printStackTrace();
+        return generateResponseTMP(GlobalExceptionCode.RUNTIME.getErrorCode(),
+                StringUtils.isBlank(ex.getMessage())?GlobalExceptionCode.RUNTIME.getDetailMessage():ex.getMessage());
+    }
+
+    @ExceptionHandler(SecurityException.class)
+    @ResponseBody
+    public ExceptionAdviceEntity securityExceptionHanlder(SecurityException ex) {
+        ex.printStackTrace();
+        return generateResponseTMP(GlobalExceptionCode.SECURITY.getErrorCode(),
+                StringUtils.isBlank(ex.getMessage())?GlobalExceptionCode.SECURITY.getDetailMessage():ex.getMessage());
+    }
+
+    @ExceptionHandler(StringIndexOutOfBoundsException.class)
+    @ResponseBody
+    public ExceptionAdviceEntity stringIndexOutOfBoundsExceptionHanlder(StringIndexOutOfBoundsException ex) {
+        ex.printStackTrace();
+        return generateResponseTMP(GlobalExceptionCode.STRING_INDEX_OUT_OF_BOUNDS.getErrorCode(),
+                StringUtils.isBlank(ex.getMessage())?GlobalExceptionCode.STRING_INDEX_OUT_OF_BOUNDS.getDetailMessage():ex.getMessage());
+    }
+
+    @ExceptionHandler(TypeNotPresentException.class)
+    @ResponseBody
+    public ExceptionAdviceEntity typeNotPresentExceptionHanlder(TypeNotPresentException ex) {
+        ex.printStackTrace();
+        return generateResponseTMP(GlobalExceptionCode.TYPE_NOT_PRESENT.getErrorCode(),
+                StringUtils.isBlank(ex.getMessage())?GlobalExceptionCode.TYPE_NOT_PRESENT.getDetailMessage():ex.getMessage());
+    }
+
+    @ExceptionHandler(UnsupportedOperationException.class)
+    @ResponseBody
+    public ExceptionAdviceEntity unsupportedOperationExceptionHanlder(UnsupportedOperationException ex) {
+        ex.printStackTrace();
+        return generateResponseTMP(GlobalExceptionCode.UNSUPPORTED_OPERATION.getErrorCode(),
+                StringUtils.isBlank(ex.getMessage())?GlobalExceptionCode.UNSUPPORTED_OPERATION.getDetailMessage():ex.getMessage());
     }
 
     @ExceptionHandler(Exception.class)
--- ragdoll-global-advice/src/main/java/tech/veedo/ragdoll/exception/GlobalExceptionCode.java
+++ ragdoll-global-advice/src/main/java/tech/veedo/ragdoll/exception/GlobalExceptionCode.java
@@ -6,7 +6,33 @@ import lombok.Getter;
 public enum GlobalExceptionCode {
 
     COMMON(300, "统一标准异常"),
-    NULL_POINTER(301, "空指针异常");
+    ARITHMETIC(301, "运算异常"),
+    ARRAY_INDEX_OUT_OF_BOUNDS(302, "索引超出边界"),
+    ARRAY_STORE(303, "数组存储对象类型不正确"),
+    CLASS_CAST(304, "类转换异常"),
+    CLASS_NOT_FOUND(305, "未找到class"),
+    CLONE_NOT_SUPPORTED(306, "对象不能被克隆"),
+    ENUM_CONSTANT_NOT_PRESENT(307, "枚举常量不存在"),
+    ILLEGAL_ACCESS(308, "没有访问权限"),
+    ILLEGAL_ARGUMENT(309, "参数不合法"),
+    ILLEGAL_MONITOR(310, "当前的线程不是此对象锁的所有者，却调用该对象的notify()，notify()，wait()等方法"),
+    ILLEGAL_STATE(311, "表示在非法或不适当的时间调用了方法"),
+    ILLEGAL_THREAD_STATE(312, "抛出以指示线程未处于所请求操作的适当状态"),
+    INDEX_OUT_OF_BOUNDS(313, "下标越界"),
+    INSTANTIATION(314, "不能实例化对象"),
+    INTERRUPTED(315, "线程不能二次阻塞"),
+    NEGATIVE_ARRAY_SIZE(316, "数组大小为负"),
+    NO_SUCH_FIELD(317, "没有找到该属性"),
+    NO_SUCH_METHOD(318, "没有找到该方法"),
+    NULL_POINTER(319, "空指针异常"),
+    NUMBER_FORMAT(320, "数字解析异常"),
+    REFLECTIVE_OPERATION(321, "核心反射中反射操作抛出的常见超类异常"),
+    RUNTIME(322, "运行时异常"),
+    SECURITY(323, "安全违规"),
+    STRING_INDEX_OUT_OF_BOUNDS(324, "字符串下标越界"),
+    TYPE_NOT_PRESENT(325, "要获取的类型未提供"),
+    UNSUPPORTED_OPERATION(326, "不支持的操作"),
+    ;
 
     private Integer errorCode;
 
