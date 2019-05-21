7f53b35b144dd66d45e2eb5c8b5b85b020018e38
张 一夫 <qwer1234=>
2019-05-21 10:14:17 +0800

    更新文件

--- munchkin-workflow/src/main/java/tech/veedo/munchkin/MunchkinWorkflowAutoConfiguration.java
+++ munchkin-workflow/src/main/java/tech/veedo/munchkin/MunchkinWorkflowAutoConfiguration.java
@@ -1,34 +1,10 @@
 package tech.veedo.munchkin;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import tech.veedo.munchkin.database.DatabaseAssistant;
-import tech.veedo.munchkin.workflow.server.inner.WorkflowManager;
 
 @Slf4j
 @Configuration
-@EnableConfigurationProperties(MunchkinWorkflowProperties.class)
 public class MunchkinWorkflowAutoConfiguration {
-
-    @Autowired
-    private MunchkinWorkflowProperties munchkinWorkflowProperties;
-
-    @Bean
-    @ConditionalOnProperty(prefix = "veedo.munchkin", name = "enabled", havingValue = "true")
-    public DatabaseAssistant generateAssistant() {
-        return new DatabaseAssistant(munchkinWorkflowProperties);
-    }
-
-    @Bean
-    @ConditionalOnMissingBean(WorkflowManager.class)
-    @ConditionalOnProperty(prefix = "veedo.munchkin", name = "enabled", havingValue = "true")
-    public WorkflowManager generateWFManager() {
-        return new WorkflowManager();
-    }
 
 }
